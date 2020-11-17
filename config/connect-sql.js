a1fc930d31944ea87f77f37dcd1e5e2936b5d640
Jay Idrees <idreesjay@gmail.com>
2020-11-17 01:08:48 -0500

    Setup mySQL connection, use process.env to protect pw

---
+++ config/connect-sql.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+// Setting up mySQL connection
+const connection=mysql.createConnection({
+
+    host:'localhost',
+    port: 3306,
+    user:'root',
+    // used .env to protect against password
+    password: process.env.local_mysql_pw,
+    database:'company_db'
+
+
+});
+
+connection.connect(function (err){
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
