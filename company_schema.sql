1d5ee017aba55a7b65a48e584141a5c4066494de
Jay Idrees <idreesjay@gmail.com>
2020-11-16 23:56:29 -0500

    creatd the employee table with MYSQL

--- company_schema.sql
+++ company_schema.sql
@@ -6,3 +6,16 @@ USE company_db;
 
 
 
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY(id),
+    -- Below are the variables created to facilitate linking
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    
+);
