5eabacbf6d5afe228d7bd4288037f1644847bb31
Jay Idrees <idreesjay@gmail.com>
2020-11-16 23:23:23 -0500

    Added gitignore, package.json, initial inquirer questions- working well

---
+++ run.js
@@ -0,0 +1,30 @@
+const inquirer=require('inquirer')
+
+async function main(){
+    let selected_task =""
+    while (selected_task !=='Exit'){
+       const response =await inquirer.prompt ([
+
+        {
+            type:'list',
+            message:'Please select the task you would like to perform',
+            name:'task',
+            choices:
+            [
+                {name:'I want to view a list of all the Employees in my company', value:'list_employees'},
+                {name:'I want to view a list all employees working under a specific manager', value:'list_employees_bymanager'},
+                {name:'I want to view all employees by their position', value:'list_employees_byposition'}
+            ],
+
+        }, // br close for the response object
+
+       ]); // br close for the inquirer prompt 
+
+       
+       selected_task=response.task
+    //    console.log(selected_task) - working fine
+
+    } // br close while loop
+
+} // br close for async function
+main()
