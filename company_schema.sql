295fb620683d6eb5e368aee04d3df02cdd2854f0
Jay Idrees <idreesjay@gmail.com>
2020-11-17 22:20:59 -0500

    updated files

--- company_schema.sql
+++ company_schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE employee(
     last_name VARCHAR(30),
     role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY(id),
+    PRIMARY KEY(id),git 
     -- Below are the variables created to facilitate linking
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
