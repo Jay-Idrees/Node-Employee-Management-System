d7d112ff1de49025cd31403fb48210add0409651
Jay Idrees <idreesjay@gmail.com>
2020-11-18 12:10:12 -0500

    Specified dependency requirement variable in Object Relational Mapper file

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+// Object relational mapping
+
+//Execution requirements: sql connection, inquirer, util, table
+const connection=require('./connect-sql')
+const util=require('util')
+const inquirer=require('inquirer')
+// Notice that I do not need to transfrom console.table requirement into a table
+require('console.table')
+
